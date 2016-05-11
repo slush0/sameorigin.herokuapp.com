bf273b9cb09e9459de79bf4c025b48c8c40873cb
slush0 <slush@satoshilabs.com>
2016-05-11 16:06:43 +0200

    Allow proxy also for some non-PGP usage

--- proxy.py
+++ proxy.py
@@ -4,6 +4,9 @@ from flask import Flask, make_response, request
 
 app = Flask(__name__)
 
+WHITELIST_URL = ('https://bitcoinfees.21.co/api/v1/fees/',)
+WHITELIST_CONTENT = ('-----BEGIN PGP SIGNED MESSAGE-----',)
+
 @app.route('/')
 def index():
     try:
@@ -11,12 +14,18 @@ def index():
     except:
         return ("Cannot fetch remote URL %s" % request.query_string, 500, {})
 
-    if '-----BEGIN PGP SIGNED MESSAGE-----' not in response.text:
-        return ("Provided URL does not contain any PGP signed message", 500, {})
-
     if len(response.text) > 100000:
         return ("Provided URL is over 100kB in size", 500, {})
 
+    if request.query_string.startswith(WHITELIST):
+        return process_response(response)
+
+    if any(x in response.text for x in WHITELIST_CONTENT):
+        return process_response(response)
+
+    return ("Provided URL does not pass whitelist conditions", 500, {})
+
+def process_response(response):
     resp = make_response(response.text)
     resp.headers['Access-Control-Allow-Origin'] = '*'
 
